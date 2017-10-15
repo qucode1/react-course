2d88693eafb8717e8b7dfe838cfdd48357ec0e5f
Yannick Panis <info@qucode.eu>
2017-10-15 23:13:32 +0200

    reducers

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -32,7 +32,12 @@ const resetCount = () => ({
   type: 'RESET'
 })
 
-const store = createStore((state = { count: 0 }, action) => {
+//
+// Reducers
+// 1. Reducers are pure functions (only determined by inputs/ do not interact with outside variables)
+// 2. Never change state or action
+
+const countReducer = (state = { count: 0 }, action) => {
   switch (action.type) {
 
     case 'INCREMENT':
@@ -50,7 +55,9 @@ const store = createStore((state = { count: 0 }, action) => {
     default:
       return state
   }
-})
+}
+
+const store = createStore(countReducer)
 
 const unsubscribe = store.subscribe(() => (
   console.log(store.getState())
